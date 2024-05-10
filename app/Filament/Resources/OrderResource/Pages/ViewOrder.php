4480547f3480a26c187220b4b7bcb376f263f1c7
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-05-10 11:00:55 -0300

    .

--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Traits\WithParcels;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewOrder extends ViewRecord
 {
+    use WithParcels;
+
     protected static string $resource = OrderResource::class;
 }
