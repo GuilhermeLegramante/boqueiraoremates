2d1f44ff2dde3bed8a027040f6a2181939f1cdb7
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-05-10 14:24:28 -0300

    .

--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -12,4 +12,15 @@ class ViewOrder extends ViewRecord
     use WithParcels;
 
     protected static string $resource = OrderResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['net_value'] = floatval($data['gross_value']) - (floatval($data['gross_value']) * floatval($data['discount_percentage'])) / 100;
+
+        $data['buyer_comission_value'] = (floatval($data['gross_value']) * floatval($data['buyer_commission'])) / 100;
+
+        $data['seller_comission_value'] = (floatval($data['gross_value']) * floatval($data['seller_commission'])) / 100;
+
+        return $data;
+    }
 }
