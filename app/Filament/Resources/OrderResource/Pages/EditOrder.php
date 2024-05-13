651b581404af4cee66ea584fcd6d44163f011334
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-05-13 14:06:29 -0300

    .

--- app/Filament/Resources/OrderResource/Pages/EditOrder.php
+++ app/Filament/Resources/OrderResource/Pages/EditOrder.php
@@ -33,14 +33,14 @@ class EditOrder extends EditRecord
 
     protected function mutateFormDataBeforeFill(array $data): array
     {
-        $data['net_value'] = floatval($data['gross_value']) - (floatval($data['gross_value']) * floatval($data['discount_percentage'])) / 100;
-        $data['net_value'] = number_format($data['net_value'], 2);
+        $netValue = floatval($data['gross_value']) - (floatval($data['gross_value']) * floatval($data['discount_percentage'])) / 100;
+        $data['net_value'] = number_format($netValue, 2);
 
-        $data['buyer_comission_value'] = (floatval($data['gross_value']) * floatval($data['buyer_commission'])) / 100;
-        $data['buyer_comission_value'] = number_format($data['buyer_comission_value'], 2);
+        $buyerComission = (floatval($data['gross_value']) * floatval($data['buyer_commission'])) / 100;
+        $data['buyer_comission_value'] = number_format($buyerComission, 2);
 
-        $data['seller_comission_value'] = (floatval($data['gross_value']) * floatval($data['seller_commission'])) / 100;
-        $data['seller_comission_value'] = number_format($data['seller_comission_value'], 2);
+        $sellerComission = (floatval($data['gross_value']) * floatval($data['seller_commission'])) / 100;
+        $data['seller_comission_value'] = number_format($sellerComission, 2);
 
         return $data;
     }
