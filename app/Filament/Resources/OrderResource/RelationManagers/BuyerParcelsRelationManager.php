ddac82473e86aa55f5a7bc6d97ed9e0410d66f4e
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-04-12 20:11:21 -0300

    Pagamento de parcelas em lote

--- app/Filament/Resources/OrderResource/RelationManagers/BuyerParcelsRelationManager.php
+++ app/Filament/Resources/OrderResource/RelationManagers/BuyerParcelsRelationManager.php
@@ -3,16 +3,20 @@
 namespace App\Filament\Resources\OrderResource\RelationManagers;
 
 use App\Filament\Tables\ParcelsTable;
+use App\Models\PaymentMethod;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\TextInputColumn;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Collection;
 
 class BuyerParcelsRelationManager extends RelationManager
 {
@@ -51,8 +55,45 @@ class BuyerParcelsRelationManager extends RelationManager
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    $this->customBulkAction(),
                 ]),
             ]);
+    }
+
+    protected function customBulkAction(): BulkAction
+    {
+        return BulkAction::make('batchPayment')
+            ->label('Pagamento em Lote')
+            ->form([
+                Forms\Components\Select::make('payment_method_id')
+                    ->label('Método de Pagamento')
+                    ->options(PaymentMethod::all()->pluck('name', 'id'))
+                    ->default(6)
+                    ->required(),
+                Forms\Components\Toggle::make('paid')
+                    ->label('Parcela Paga')
+                    ->inline(false)
+                    ->default(true),
+                Forms\Components\Textarea::make('note')
+                    ->label('Observação')
+                    ->maxLength(255),
+            ])
+            ->action(function (array $data, Collection $records) {
+                foreach ($records as $record) {
+                    $record->update([
+                        'payment_method_id' => $data['payment_method_id'],
+                        'paid' => $data['paid'],
+                        'note' => $data['note'],
+                    ]);
+                }
+
+                Notification::make()
+                    ->title('Pagamento de Parcelas atualizado com sucesso!')
+                    ->success()
+                    ->send();
+            })
+            ->modalHeading('Pagamento de Parcelas em Lote')
+            ->modalSubmitActionLabel('Salvar')
+            ->requiresConfirmation();
     }
 }
--- app/Filament/Resources/OrderResource/RelationManagers/ParcelsRelationManager.php
+++ app/Filament/Resources/OrderResource/RelationManagers/ParcelsRelationManager.php
@@ -16,10 +16,13 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
+use Filament\Notifications\Notification;
 use Filament\Support\Enums\Alignment;
+use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextInputColumn;
 use Filament\Tables\Columns\ToggleColumn;
+use Illuminate\Support\Collection;
 
 class ParcelsRelationManager extends RelationManager
 {
@@ -57,11 +60,46 @@ class ParcelsRelationManager extends RelationManager
                 // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                // Tables\Actions\BulkActionGroup::make([
-                //     Tables\Actions\DeleteBulkAction::make(),
-                // ]),
+                Tables\Actions\BulkActionGroup::make([
+                    $this->customBulkAction(),
+                ]),
             ]);
     }
 
-   
+    protected function customBulkAction(): BulkAction
+    {
+        return BulkAction::make('batchPayment')
+            ->label('Pagamento em Lote')
+            ->form([
+                Forms\Components\Select::make('payment_method_id')
+                    ->label('Método de Pagamento')
+                    ->options(PaymentMethod::all()->pluck('name', 'id'))
+                    ->default(6)
+                    ->required(),
+                Forms\Components\Toggle::make('paid')
+                    ->label('Parcela Paga')
+                    ->inline(false)
+                    ->default(true),
+                Forms\Components\Textarea::make('note')
+                    ->label('Observação')
+                    ->maxLength(255),
+            ])
+            ->action(function (array $data, Collection $records) {
+                foreach ($records as $record) {
+                    $record->update([
+                        'payment_method_id' => $data['payment_method_id'],
+                        'paid' => $data['paid'],
+                        'note' => $data['note'],
+                    ]);
+                }
+
+                Notification::make()
+                    ->title('Pagamento de Parcelas atualizado com sucesso!')
+                    ->success()
+                    ->send();
+            })
+            ->modalHeading('Pagamento de Parcelas em Lote')
+            ->modalSubmitActionLabel('Salvar')
+            ->requiresConfirmation();
+    }
 }
