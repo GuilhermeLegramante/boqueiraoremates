c9edab5eacc82db318d88cb91c206c3e5dac327d
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-05-06 12:35:34 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -38,95 +38,11 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/extrato-do-vendedor/pdf', [SellerStatementController::class, 'getPdf'])->name('seller-statement-pdf');
 });
 
-// Route::get('/ficha-cadastral/{id}', [ReportController::class, 'clientDetails']);
-
 Route::get('/teste/{id}', function (string $id) {
     return 'User ' . $id;
 });
 
 Route::get('/salvar-datas-santavitoria', function () {
-    // set_time_limit(0);
-
-    // $file = file_get_contents('C:\Users\Marca & Sinal\Desktop\santa vitoria do palmar\ajuste-datas\marcas-sem-cabecalho.txt');
-
-    // $array = explode(PHP_EOL, $file);
-
-    // foreach ($array as $key => $value) {
-    //     if (empty($value)) continue;
-
-    //     $exploit = explode("\t", $value);
-
-    //     $brandId = $exploit[0];
-    //     $date = $exploit[5];
-
-    //     $brand = DB::connection('marcaesinal')->table('agro_marca')
-    //         ->where('id', $brandId)
-    //         ->get()
-    //         ->first();
-
-    //     if (isset($brand) && isset($date)) {
-    //         if ($brandId > 6118) {
-    //             DB::connection('marcaesinal')->table('agro_marca')
-    //                 ->where('id', $brandId)
-    //                 ->update([
-    //                     'datahora' => $date,
-    //                 ]);
-    //             Log::info("Id Marca: {$brandId} - {$date}");
-    //         }
-    //     }
-    // }
-
-    dd('salvou as datas');
-});
-
-
-Route::get('/gerando-log-conversao', function () {
-    // set_time_limit(0);
-
-    // $file = file_get_contents('C:\Users\Marca & Sinal\Desktop\CONVERSAO_ALEGRETE_MeS\CSVs\OK\marcas_e_produtores.csv');
-
-    // $array = explode(PHP_EOL, $file);
-
-    // // Itera sobre as linhas do CSV
-    // foreach ($array as $key => $value) {
-    //     // Se a linha estiver vazia, pula
-    //     if (empty($value)) continue;
-
-    //     // Divide a linha do CSV em partes
-    //     $exploit = explode(',', $value);
-
-    //     $nomeImagem = trim($exploit[5]);
-
-    //     $brand_and_farmer_data['id_marca'] = isset($exploit[0]) && trim($exploit[0]) != 'NULL' ? trim($exploit[0]) : null;
-    //     $brand_and_farmer_data['nome_imagem_marca'] = isset($exploit[5]) && trim($exploit[5]) != 'NULL'
-    //         ? substr(trim($exploit[5]), 0, -4)
-    //         : null;
-    //     $brand_and_farmer_data['id_produtor'] = isset($exploit[8]) && trim($exploit[8]) != 'NULL' ? trim($exploit[8]) : null;
-    //     $brand_and_farmer_data['nome_produtor'] = isset($exploit[9]) && trim($exploit[9]) != 'NULL' ? trim($exploit[9]) : null;
-
-    //     if (!file_exists("C:\Users\Marca & Sinal\Desktop\CONVERSAO_ALEGRETE_MeS\marcas-png\\" . $brand_and_farmer_data['nome_imagem_marca'] . ".png")) {
-    //         Log::error("Arquivo não encontrado: https://ecidade.alegrete.rs.gov.br/w/3/tmp/{$nomeImagem}");
-    //     } else if (!isset($brand_and_farmer_data['nome_produtor'])) {
-    //         Log::error("Campo nome_produtor nulo. ID Produtor: {$brand_and_farmer_data['id_produtor']}");
-    //     } else if (!isset($brand_and_farmer_data['nome_imagem_marca'])) {
-    //         Log::error("Campo nome_imagem_marca nulo. ID Marca: {$brand_and_farmer_data['id_marca']}");
-    //     } else {
-    //         // Busca a marca
-    //         $brand = DB::connection('marcaesinal')->table('agro_marca')
-    //             ->join('hscad_cadmunicipal', 'hscad_cadmunicipal.inscricaomunicipal', '=', 'agro_marca.idmunicipe')
-    //             ->select('agro_marca.id AS id', 'hscad_cadmunicipal.nome AS nome', 'agro_marca.path as path')
-    //             ->where('hscad_cadmunicipal.nome', 'like', $brand_and_farmer_data['nome_produtor'])
-    //             ->get()
-    //             ->first();
-
-    //         if (isset($brand)) {
-    //             Log::info("Id Marca: {$brand->id} - {$brand->nome} - {$brand->path}");
-    //         }
-    //     }
-    // }
-});
-
-
 
 Route::get('/', function () {
     return redirect(route('filament.admin.pages.dashboard'));
