d983a6dcd96f9c5e35b50a842892ca4dc5b4e05a
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-10-17 10:24:01 -0300

    .

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,6 +5,6 @@
 @section('content')
     @include('site.banners')
     @include('site.events')
-    @include('site.team')
+    {{-- @include('site.team') --}}
     @include('site.contact')
 @endsection
