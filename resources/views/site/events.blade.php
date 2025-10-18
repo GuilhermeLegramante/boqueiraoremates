a4b6bf4b7cbfe2bd54be5dd30dc7cc40260ad015
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-10-17 21:00:21 -0300

    .

--- resources/views/site/events.blade.php
+++ resources/views/site/events.blade.php
@@ -35,19 +35,19 @@
                     </p>
                 </div>
 
-                <!-- Pré-lance e Regulamento lado a lado, sem fundo -->
-                <div class="flex justify-center md:justify-start items-start gap-6 mt-2">
+                <!-- Pré-lance e Regulamento lado a lado -->
+                <div class="flex justify-center md:justify-start items-start gap-6 mt-0">
                     <!-- Pré-lance -->
                     <a href="{{ route('events.show', $event) }}">
                         <img src="{{ asset('img/prelance.png') }}" alt="Pré-lance"
-                            class="w-64 h-56 object-contain rounded-xl shadow-md hover:scale-105 transition-transform duration-300">
+                            class="w-60 md:w-72 h-auto object-contain rounded-xl shadow-md hover:scale-105 transition-transform duration-300">
                     </a>
 
                     <!-- Regulamento -->
                     @if ($event->regulation)
                         <a href="{{ asset('storage/' . $event->regulation) }}" target="_blank">
                             <img src="{{ asset('img/regulamento.png') }}" alt="Regulamento"
-                                class="w-64 h-56 object-contain rounded-xl shadow-md hover:scale-105 transition-transform duration-300">
+                                class="w-60 md:w-72 h-auto object-contain rounded-xl shadow-md hover:scale-105 transition-transform duration-300">
                         </a>
                     @endif
                 </div>
