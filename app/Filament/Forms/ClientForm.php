93e280b761e57efc23a0e3eafa2b1b344f91acf0
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-10-20 21:23:33 -0300

    .

--- app/Filament/Forms/ClientForm.php
+++ app/Filament/Forms/ClientForm.php
@@ -319,7 +319,7 @@ class ClientForm
             TextInput::make('email')
                 ->label(__('filament-panels::pages/auth/register.form.email.label'))
                 ->email()
-                ->required()
+                // ->required()
                 ->maxLength(255)
                 ->unique(table: 'users', column: 'email'),
 
