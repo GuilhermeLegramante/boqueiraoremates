67a0093213d9a4c58baca5797acadcbdfdbde8f2
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-10-24 23:08:48 -0300

    .

--- resources/views/filament/pages/first-password-change.blade.php
+++ resources/views/filament/pages/first-password-change.blade.php
@@ -1,4 +1,4 @@
-<x-filament-panels::page.simple>
+{{-- <x-filament-panels::page.simple>
     <x-slot name="heading">
         <img src="{{ asset('logo.png') }}" class="mx-auto h-16 mb-4" alt="Boqueirão Remates">
         <h2 class="text-2xl font-bold text-center text-white">
@@ -22,4 +22,24 @@
             Salvar nova senha
         </x-filament::button>
     </div>
-</x-filament-panels::page.simple>
+</x-filament-panels::page.simple> --}}
+
+<section class="grid auto-cols-fr gap-y-8 py-8">
+    <div>
+        <header class="fi-header flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between ">
+            <div>
+                <h1
+                    class="fi-header-heading text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3xl">
+                    Perfil
+                </h1>
+            </div>
+        </header>
+
+        <x-filament-panels::form wire:submit="save">
+            <br>
+            {{ $this->form }}
+
+            <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" alignment="right" />
+        </x-filament-panels::form>
+    </div>
+</section>
