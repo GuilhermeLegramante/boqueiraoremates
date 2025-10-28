aec7aeec1ad52099c6a1b2c18cb5aa5961cddb70
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-10-28 12:16:24 -0300

    .

--- resources/views/site/events.blade.php
+++ resources/views/site/events.blade.php
@@ -30,8 +30,10 @@
             <div class="bg-[#4D6766] p-4 grid grid-cols-1 md:grid-cols-5 gap-4 items-center text-white">
                 <!-- Imagem do evento -->
                 <div class="aspect-[4/3] w-44 rounded overflow-hidden bg-black">
-                    <img src="{{ asset('storage/' . $event->banner_min) }}" alt="Logo do lote"
-                        class="object-cover w-full h-full">
+                    <a href="{{ route('events.show', $event) }}">
+                        <img src="{{ asset('storage/' . $event->banner_min) }}" alt="Logo do lote"
+                            class="object-cover w-full h-full">
+                    </a>
                 </div>
 
 
