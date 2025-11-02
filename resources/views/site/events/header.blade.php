fd8acd067af4d92b123b465e985539f9754d59ac
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-02 15:21:29 -0300

    .

--- resources/views/site/events/header.blade.php
+++ resources/views/site/events/header.blade.php
@@ -65,9 +65,11 @@
         </div>
 
         <!-- Observações -->
-        <div class="bg-white/10 rounded-xl p-5 text-sm md:text-base leading-relaxed shadow-md mt-6 lg:mt-0">
+        <div
+            class="bg-white/10 rounded-xl p-5 text-sm md:text-base leading-relaxed shadow-md mt-6 lg:mt-0 text-justify">
             <p>{{ $event->note }}</p>
         </div>
+
     </div>
 
     <!-- Modais permanecem iguais -->
