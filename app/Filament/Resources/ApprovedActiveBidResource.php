0e0e7c7e009df32ba2ff99b70d6351bb582e44dd
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-03 12:56:31 -0300

    .

--- app/Filament/Resources/ApprovedActiveBidResource.php
+++ app/Filament/Resources/ApprovedActiveBidResource.php
@@ -47,7 +47,7 @@ class ApprovedActiveBidResource extends Resource
                 return $query;
             })
             ->header(function () {
-                return view('components.filament-tables.bid-filters-pro', [
+                return view('components.filament-tables.bid-filters', [
                     'eventsQuery' => \App\Models\Event::query()->where('published', true),
                     'lotsQuery' => \App\Models\AnimalEvent::query(),
                     'usersQuery' => \App\Models\User::query(),
