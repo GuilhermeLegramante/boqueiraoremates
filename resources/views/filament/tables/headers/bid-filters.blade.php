62ecc53b68a4b05290cb471fcd6ba13d3590cd2d
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-03 14:46:15 -0300

    .

--- resources/views/filament/tables/headers/bid-filters.blade.php
+++ resources/views/filament/tables/headers/bid-filters.blade.php
@@ -72,11 +72,14 @@
     </div>
 
     {{-- Limpar filtros --}}
-    <div class="flex items-end">
-        <button type="button"
-            onclick="window.location.href='{{ route('filament.filters.update') }}?clear={{ $resource }}'"
+    <form method="POST" action="{{ route('filament.filters.update') }}">
+        @csrf
+        <input type="hidden" name="resource" value="{{ $resource }}">
+        <input type="hidden" name="clear" value="1">
+        <button type="submit"
             class="px-4 py-2 rounded-lg text-sm font-semibold text-gray-700 dark:text-gray-200 bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 border border-gray-300 dark:border-gray-600 transition-colors duration-150">
             Limpar filtros
         </button>
-    </div>
+    </form>
+
 </form>
