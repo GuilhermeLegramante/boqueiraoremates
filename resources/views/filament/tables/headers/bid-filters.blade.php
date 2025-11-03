5ccea3e7595c254d0a119b7a0273096e1926ef09
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-03 14:35:29 -0300

    .

--- resources/views/filament/tables/headers/bid-filters.blade.php
+++ resources/views/filament/tables/headers/bid-filters.blade.php
@@ -76,6 +76,10 @@
     </div>
 </form>
 
+<!-- TomSelect CSS e JS via CDN -->
+<link href="https://cdn.jsdelivr.net/npm/tom-select/dist/css/tom-select.bootstrap5.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/tom-select/dist/js/tom-select.complete.min.js"></script>
+
 <script>
     function updateLots(eventId) {
         lotSelect.clearOptions();
