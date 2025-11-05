9e04b8432d7a5a0c7bd0235c80172a378c33e34d
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-05 10:45:23 -0300

    .

--- resources/views/site/banners.blade.php
+++ resources/views/site/banners.blade.php
@@ -1,22 +1,20 @@
-<!-- Carrossel de Banners -->
-<section class="relative pt-32 md:pt-48 bg-green-900 bg-repeat-x bg-top flex items-center justify-center"
-    style="background-image: url('{{ asset('img/madeira.png') }}'); background-size: auto 100%;">
-    <div class="absolute inset-0 bg-black/50"></div>
-    <div class="relative z-10 w-full max-w-[930px] mx-auto px-4">
-        <div class="swiper-container rounded-2xl shadow-2xl overflow-hidden">
-            <div class="swiper-wrapper">
-                @foreach ($events as $event)
-                    <div class="swiper-slide flex justify-center items-center bg-black">
-                        <a href="{{ route('events.show', $event) }}" class="block w-full">
-                            <div class="w-full" style="aspect-ratio: 930 / 340;">
-                                <img src="{{ asset("storage/{$event->banner}") }}" alt="Banner"
-                                    class="w-full h-full object-cover object-center">
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="swiper-pagination"></div>
-        </div>
-    </div>
-</section>
+ <!-- Carrossel de Banners -->
+ <section class="relative h-[500px] pt-32 md:pt-48 bg-green-900 bg-repeat-x bg-top flex items-center justify-center"
+     style="background-image: url('{{ asset('img/madeira.png') }}'); background-size: auto 100%;">
+     <div class="absolute inset-0 bg-black/50"></div>
+     <div class="relative z-10 max-w-[930px] w-full mx-auto px-4">
+         <div class="swiper-container rounded-2xl shadow-2xl overflow-hidden">
+             <div class="swiper-wrapper">
+                 @foreach ($events as $event)
+                     <div class="swiper-slide relative flex justify-center">
+                         <a href="{{ route('events.show', $event) }}">
+                             <img src="{{ asset("storage/{$event->banner}") }}" alt="Banner"
+                                 class="max-w-full max-h-[400px] object-contain">
+                         </a>
+                     </div>
+                 @endforeach
+             </div>
+             <div class="swiper-pagination"></div>
+         </div>
+     </div>
+ </section>
