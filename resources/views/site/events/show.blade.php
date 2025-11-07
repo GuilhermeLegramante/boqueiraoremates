bc9d94c19094528323c9ef3ec505e93f072dfbea
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-07 11:47:08 -0300

    .

--- resources/views/site/events/show.blade.php
+++ resources/views/site/events/show.blade.php
@@ -106,7 +106,13 @@
 
                                 <div
                                     class="grid grid-cols-[140px_1fr] items-center gap-2 text-gray-200 font-extrabold text-md mb-4">
-                                    @if (floatval($animal->current_bid) > 0)
+                                    @if ($status == 'vendido')
+                                        <span>Lote:</span>
+                                        <span
+                                            class="inline-block bg-red-600 text-white px-3 py-1 rounded-lg shadow text-center min-w-[110px]">
+                                            VENDIDO
+                                        </span>
+                                    @elseif (floatval($animal->current_bid) > 0)
                                         <span>Lance Atual:</span>
                                         <span
                                             class="inline-block bg-green-600 text-white px-3 py-1 rounded-lg shadow text-right min-w-[110px]">
@@ -120,20 +126,23 @@
                                         </span>
                                     @endif
 
-                                    @if (floatval($animal->pivot->target_value) > 0)
-                                        <span>Lance Alvo:</span>
-                                        <span
-                                            class="inline-block bg-yellow-500 text-black px-3 py-1 rounded-lg shadow text-right min-w-[110px]">
-                                            R$ {{ number_format($animal->pivot->target_value, 2, ',', '.') }}
-                                        </span>
-                                    @else
-                                        <span>&nbsp;</span>
-                                        <span
-                                            class="inline-block px-3 py-1 rounded-lg text-transparent min-w-[110px] select-none">
-                                            R$ 0,00
-                                        </span>
+                                    @if ($status != 'vendido')
+                                        @if (floatval($animal->pivot->target_value) > 0)
+                                            <span>Lance Alvo:</span>
+                                            <span
+                                                class="inline-block bg-yellow-500 text-black px-3 py-1 rounded-lg shadow text-right min-w-[110px]">
+                                                R$ {{ number_format($animal->pivot->target_value, 2, ',', '.') }}
+                                            </span>
+                                        @else
+                                            <span>&nbsp;</span>
+                                            <span
+                                                class="inline-block px-3 py-1 rounded-lg text-transparent min-w-[110px] select-none">
+                                                R$ 0,00
+                                            </span>
+                                        @endif
                                     @endif
                                 </div>
+
 
                                 <a href="{{ route('animals.show', [$event->id, $animal->pivot->id]) }}"
                                     class="mt-4 inline-block w-full text-center bg-[#003333] text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-[#005050] transition">
