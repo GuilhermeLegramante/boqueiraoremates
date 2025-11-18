8640601d33ab4b7237baed31c1cd748fc0a466c8
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-11-18 12:07:54 -0300

    .

--- resources/views/site/animals/show.blade.php
+++ resources/views/site/animals/show.blade.php
@@ -129,113 +129,112 @@
 
                     @if (!$event->closed)
                         <!-- Card de lance -->
-                        <div class="bg-[#002222] p-6 rounded-xl shadow-lg mt-6">
-                            <h3 class="text-xl font-bold mb-4">Dar lance</h3>
+                        @if ($animal->pivot->status === 'disponivel' && !$event->closed)
+                            <div class="bg-[#002222] p-6 rounded-xl shadow-lg mt-6">
+                                <h3 class="text-xl font-bold mb-4">Dar lance</h3>
 
-                            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-6">
-                                <div class="bg-[#003333] shadow-lg rounded-lg p-4">
-                                    <p class="text-green-300">Lance atual</p>
-                                    <p class="text-xl font-bold text-white">
-                                        R$ {{ number_format($animal->current_bid ?? 0, 2, ',', '.') }}
-                                    </p>
-                                </div>
-
-                                <div class="bg-[#003333] shadow-lg rounded-lg p-4">
-                                    <p class="text-green-300">Próximo lance mínimo</p>
-                                    <p class="text-xl font-bold text-white">
-                                        R$ {{ number_format($animal->next_bid ?? 0, 2, ',', '.') }}
-                                    </p>
-                                </div>
-
-                                <div class="bg-[#003333] shadow-lg rounded-lg p-4">
-                                    <p class="text-green-300">Lance-alvo</p>
-                                    @if ($animal->pivot->target_value && $animal->pivot->target_value != '0.00')
-                                        <p class="text-xl font-bold text-green-300">
-                                            R$ {{ number_format($animal->pivot->target_value, 2, ',', '.') }}
+                                <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-6">
+                                    <div class="bg-[#003333] shadow-lg rounded-lg p-4">
+                                        <p class="text-green-300">Lance atual</p>
+                                        <p class="text-xl font-bold text-white">
+                                            R$ {{ number_format($animal->current_bid ?? 0, 2, ',', '.') }}
                                         </p>
-                                    @else
-                                        <p class="text-xl font-bold text-green-300">Consulte o regulamento</p>
-                                    @endif
-                                </div>
-                            </div>
+                                    </div>
 
-                            {{-- Usuário logado --}}
-                            @auth
-                                @php
-                                    $client = Auth::user()->client;
-                                @endphp
+                                    <div class="bg-[#003333] shadow-lg rounded-lg p-4">
+                                        <p class="text-green-300">Próximo lance mínimo</p>
+                                        <p class="text-xl font-bold text-white">
+                                            R$ {{ number_format($animal->next_bid ?? 0, 2, ',', '.') }}
+                                        </p>
+                                    </div>
 
-                                {{-- Pode dar lance somente se estiver disponível e evento não fechado --}}
-                                @if ($animal->pivot->status === 'disponivel' && !$event->closed)
-                                    {{-- Cliente está apto --}}
-                                    @if ($client && $client->situation === 'able')
-                                        {{-- FORMULÁRIO DE LANCE --}}
-                                        <form action="{{ route('bids.store') }}" method="POST" class="space-y-4"
-                                            id="bidForm">
-                                            @csrf
-
-                                            <input type="hidden" name="event_id" value="{{ $event->id }}">
-                                            <input type="hidden" name="animal_event_id" value="{{ $animal->pivot->id }}">
-
-                                            <div class="relative">
-                                                <span
-                                                    class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700 z-10">R$</span>
-                                                <input type="text" name="amount" id="bidInput"
-                                                    class="w-full pl-10 px-4 py-2 rounded-lg text-black border"
-                                                    placeholder="0,00" required>
-                                            </div>
-
-                                            <p id="bidError" class="text-red-600 text-sm mt-1 hidden">
-                                                O valor do lance não pode ser menor que o lance mínimo
-                                                (R$ {{ number_format($animal->next_bid, 2, ',', '.') }})
-                                                .
+                                    <div class="bg-[#003333] shadow-lg rounded-lg p-4">
+                                        <p class="text-green-300">Lance-alvo</p>
+                                        @if ($animal->pivot->target_value && $animal->pivot->target_value != '0.00')
+                                            <p class="text-xl font-bold text-green-300">
+                                                R$ {{ number_format($animal->pivot->target_value, 2, ',', '.') }}
                                             </p>
+                                        @else
+                                            <p class="text-xl font-bold text-green-300">Consulte o regulamento</p>
+                                        @endif
+                                    </div>
+                                </div>
+                        @endif
 
-                                            <button type="submit"
-                                                class="w-full bg-green-600 text-white font-bold px-6 py-3 rounded-lg shadow hover:bg-green-500 transition">
-                                                Confirmar Lance
-                                            </button>
-                                        </form>
+                        {{-- Usuário logado --}}
+                        @auth
+                            @php
+                                $client = Auth::user()->client;
+                            @endphp
 
-                                        @include('site.animals.bid-modal')
-                                    @else
-                                        {{-- Cliente não apto --}}
-                                        <div
-                                            class="bg-yellow-200 text-yellow-900 p-4 rounded-lg border border-yellow-300 shadow-sm">
-                                            <p class="font-semibold">⚠️ Para dar lance, você precisa estar logado.</p>
-                                            <p class="text-sm mt-2"> Ainda não possui cadastro? <a
-                                                    href="{{ route('filament.admin.auth.register') }}"
-                                                    class="inline-block mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-lg shadow transition">
-                                                    Criar cadastro </a> </p>
+                            {{-- Pode dar lance somente se estiver disponível e evento não fechado --}}
+                            @if ($animal->pivot->status === 'disponivel' && !$event->closed)
+                                {{-- Cliente está apto --}}
+                                @if ($client && $client->situation === 'able')
+                                    {{-- FORMULÁRIO DE LANCE --}}
+                                    <form action="{{ route('bids.store') }}" method="POST" class="space-y-4" id="bidForm">
+                                        @csrf
+
+                                        <input type="hidden" name="event_id" value="{{ $event->id }}">
+                                        <input type="hidden" name="animal_event_id" value="{{ $animal->pivot->id }}">
+
+                                        <div class="relative">
+                                            <span
+                                                class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-700 z-10">R$</span>
+                                            <input type="text" name="amount" id="bidInput"
+                                                class="w-full pl-10 px-4 py-2 rounded-lg text-black border" placeholder="0,00"
+                                                required>
                                         </div>
-                                    @endif
+
+                                        <p id="bidError" class="text-red-600 text-sm mt-1 hidden">
+                                            O valor do lance não pode ser menor que o lance mínimo
+                                            (R$ {{ number_format($animal->next_bid, 2, ',', '.') }})
+                                            .
+                                        </p>
+
+                                        <button type="submit"
+                                            class="w-full bg-green-600 text-white font-bold px-6 py-3 rounded-lg shadow hover:bg-green-500 transition">
+                                            Confirmar Lance
+                                        </button>
+                                    </form>
+
+                                    @include('site.animals.bid-modal')
                                 @else
-                                    {{-- Animal não disponível para lance --}}
+                                    {{-- Cliente não apto --}}
                                     <div
                                         class="bg-yellow-200 text-yellow-900 p-4 rounded-lg border border-yellow-300 shadow-sm">
-                                        <p class="font-semibold">⚠️ Este lote não está disponível para lance.</p>
+                                        <p class="font-semibold">⚠️ Para dar lance, você precisa estar logado.</p>
+                                        <p class="text-sm mt-2"> Ainda não possui cadastro? <a
+                                                href="{{ route('filament.admin.auth.register') }}"
+                                                class="inline-block mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-lg shadow transition">
+                                                Criar cadastro </a> </p>
                                     </div>
                                 @endif
-                            @endauth
-
-                            {{-- Visitante --}}
-                            @guest
+                            @else
+                                {{-- Animal não disponível para lance --}}
                                 <div class="bg-yellow-200 text-yellow-900 p-4 rounded-lg border border-yellow-300 shadow-sm">
-                                    <p class="font-semibold">⚠️ Você deve estar logado para dar lances.</p>
-                                    <a href="{{ route('login') }}"
-                                        class="inline-block mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-lg shadow transition">
-                                        Clique aqui para logar
-                                    </a>
+                                    <p class="font-semibold">⚠️ Este lote não está disponível para lance.</p>
                                 </div>
-                            @endguest
+                            @endif
+                        @endauth
 
-                        </div>
+                        {{-- Visitante --}}
+                        @guest
+                            <div class="bg-yellow-200 text-yellow-900 p-4 rounded-lg border border-yellow-300 shadow-sm">
+                                <p class="font-semibold">⚠️ Você deve estar logado para dar lances.</p>
+                                <a href="{{ route('login') }}"
+                                    class="inline-block mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black font-semibold rounded-lg shadow transition">
+                                    Clique aqui para logar
+                                </a>
+                            </div>
+                        @endguest
                     @endif
-
-
                 </div>
 
+
             </div>
+
+        </div>
         </div>
 
         <!-- Navegação entre lotes -->
