0ecd8fc63008ebf4008d0bbfa2e048aa19b112f1
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-01-29 13:27:08 -0300

    .

--- resources/views/site/events/show.blade.php
+++ resources/views/site/events/show.blade.php
@@ -105,22 +105,25 @@
                                         </span> --}}
 
                                         <div
-                                            class="absolute left-4 bottom-4 flex flex-col items-center justify-center
-               w-28 h-28 rounded-full bg-white border-4 border-red-700
-               shadow-2xl transform -rotate-6 origin-left select-none">
+                                            class="absolute left-4 bottom-4 w-28 h-28
+               rounded-full bg-white border-[5px] border-red-700
+               shadow-2xl transform -rotate-6 origin-left
+               flex items-center justify-center select-none">
 
-                                            {{-- Texto LOTE (acima do retângulo) --}}
+                                            {{-- Texto LOTE (topo do círculo) --}}
                                             <span
-                                                class="mb-1 text-xs font-extrabold uppercase tracking-widest
+                                                class="absolute top-2 text-xs font-black uppercase tracking-[0.25em]
                    text-red-700">
-                                                Lote
+                                                LOTE
                                             </span>
 
-                                            {{-- Retângulo central maior --}}
+                                            {{-- Faixa atravessando --}}
                                             <span
-                                                class="px-6 py-2 text-xl font-extrabold uppercase tracking-wider
-                   text-red-700 bg-white border-4 border-red-700 rounded-md">
-                                                Vendido
+                                                class="absolute inset-x-[-20%] flex items-center justify-center
+                   py-2 text-xl font-black uppercase tracking-wider
+                   text-red-700 bg-white
+                   border-[5px] border-red-700 rounded-md">
+                                                VENDIDO
                                             </span>
                                         </div>
                                     @else
