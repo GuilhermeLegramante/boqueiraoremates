39c23cc266d4fad6cfc80b7b46f6f7d547e265c4
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-04-07 16:37:17 -0300

    .

--- app/Filament/Pages/BidReport.php
+++ app/Filament/Pages/BidReport.php
@@ -9,6 +9,8 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 
 class BidReport extends Page implements HasForms
 {
@@ -27,6 +29,42 @@ class BidReport extends Page implements HasForms
     {
         // Inicializa o formulário com dados vazios
         $this->form->fill();
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('sortear')
+                ->label('Sortear Ganhador')
+                ->color('warning')
+                ->icon('heroicon-m-gift')
+                ->requiresConfirmation()
+                ->modalHeading('Sortear entre os lances aprovados')
+                ->modalDescription('O sistema escolherá aleatoriamente um dos clientes da lista abaixo.')
+                ->modalSubmitActionLabel('Realizar Sorteio')
+                ->action(function () {
+                    $bids = $this->getBidsProperty();
+
+                    if ($bids->isEmpty()) {
+                        Notification::make()
+                            ->title('Nenhum lance encontrado')
+                            ->danger()
+                            ->send();
+                        return;
+                    }
+
+                    // Pega um lance aleatório
+                    $sorteado = $bids->random();
+
+                    // Dispara um alerta de sucesso com o nome do ganhador
+                    Notification::make()
+                        ->title('🎉 Temos um vencedor!')
+                        ->body("O cliente sorteado foi: **{$sorteado->user->name}**\n\nLote: {$sorteado->lot_number}")
+                        ->success()
+                        ->persistent() // O alerta não some sozinho
+                        ->send();
+                }),
+        ];
     }
 
     public function form(Form $form): Form
@@ -55,6 +93,12 @@ class BidReport extends Page implements HasForms
         return Bid::where('event_id', $eventId)
             ->where('status', 1) // Certifica-te que 'approved' é o status correto no teu banco
             ->with(['user', 'event'])
+            ->whereHas('user', function ($query) {
+                $query->whereNotIn('name', [
+                    '%LUIS EMERSON HOISLER DA ROSA%',
+                    '%LEANDRO CESAR DORNELES DE OLIVEIRA%'
+                ]);
+            })
             ->get();
     }
 }
