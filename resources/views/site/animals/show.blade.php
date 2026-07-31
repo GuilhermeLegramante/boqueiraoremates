ebd9a60557b4a705e99151cc338e7d3b0287d79c
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-07-31 15:25:36 -0300

    .

--- resources/views/site/animals/show.blade.php
+++ resources/views/site/animals/show.blade.php
@@ -201,9 +201,6 @@
                              Se o evento é permanente, então o target_value deve ser maior que zero.  --}}
                         @if ($isAvailable && $isEventOpen && $canParticipateInPermanentEvent)
 
-                            <p>{{ $isAvailable ? 'Disponível' : 'Indisponível' }}</p>
-                            <p>{{ $isEventOpen ? 'Evento aberto' : 'Evento fechado' }}</p>
-                            <p>{{ $canParticipateInPermanentEvent ? 'Pode participar' : 'Não pode participar' }}</p>
 
                             {{-- Cliente está apto --}}
                             @if ($client && $client->situation === 'able')
